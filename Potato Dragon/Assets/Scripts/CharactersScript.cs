7ad0e8aaf2f900cf69ed73acc75de250963e0d9d
ezelking-LAPTOP <timroberson@live.nl>
2018-01-27 09:53:55 +0100

    Script added for character spawning and flight departure system added

---
+++ Potato Dragon/Assets/Scripts/CharactersScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersScript : MonoBehaviour {
+    public int charactersToSpawn = 5;
+
+    List<CharacterScript> spawnedCharacters;
+
+    public GameObject characterTemplate;
+    public Sprite[] sprites;
+
+    // Use this for initialization
+    void Start () {
+        spawnedCharacters = new List<CharacterScript>();
+        for (int i = 0; i < charactersToSpawn; i++) {
+            GameObject spawnedCharacter = Instantiate(characterTemplate, transform);
+            spawnedCharacter.GetComponent<SpriteRenderer>().sprite = (Sprite)sprites[Random.Range(0,sprites.Length)];
+            CharacterScript newCharacter = spawnedCharacter.GetComponent<CharacterScript>();
+            spawnedCharacters.Add(newCharacter);
+            newCharacter.SetFlight(GetComponent<FlightsScript>().ChooseFlight());
+        }
+        ChooseTarget();
+	}
+
+    //Choose a random charactrer to be the suspect
+    void ChooseTarget() {
+        spawnedCharacters[Random.Range(0, spawnedCharacters.Count)].target = true;
+    }
+}
