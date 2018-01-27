c80cbd68882048abeb68d296c625c3a5079b5577
Tak man Wong <tak.man.wong@hva.nl>
2018-01-27 10:31:15 +0100

    add text on click and add text cycle for left click and return key

---
+++ Potato Dragon/Assets/Scripts/TextBehaviourScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TextBehaviourScript : MonoBehaviour
+{
+	public GameObject textBox;
+
+	public Text theText;
+	public TextAsset textFile;
+
+	public string[] textLines;
+
+	public int currentLine;
+	public int endAtLine;
+
+	public PlayerController player;
+	
+	// Function for mouseclicks
+	private void Start()
+	{
+		Init();
+	}
+
+	private void Init()
+	{
+		if (textFile != null)
+		{
+			textLines = (textFile.text.Split('\n'));
+		}
+
+		if (endAtLine == 0)
+		{
+			endAtLine = textLines.Length - 1;
+		}
+	}
+
+	private void Update()
+	{
+		theText.text = textLines[currentLine];
+
+		if (Input.GetMouseButtonDown(0) && !textBox.activeSelf && currentLine < endAtLine)
+		{
+			Debug.Log("poep");
+			if (textFile == null) return;
+			textBox.SetActive(true);
+			Init();
+		} else if (Input.GetKeyDown(KeyCode.Return) && currentLine < endAtLine)
+		{
+			currentLine++;
+		} else if (Input.GetKeyDown(KeyCode.Return) && currentLine >= endAtLine)
+		{
+			textBox.SetActive(false);
+		} else if (Input.GetMouseButtonDown(0) && currentLine < endAtLine)
+		{
+			currentLine++;
+		} else if (Input.GetMouseButtonDown(0) && currentLine >= endAtLine)
+		{
+			textBox.SetActive(false);
+		}
+	}
+}
