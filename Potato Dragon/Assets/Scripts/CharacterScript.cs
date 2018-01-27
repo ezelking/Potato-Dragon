4f8d5d2bd0bb72ae6031527ddce3aacb3357be5b
ezelking-LAPTOP <timroberson@live.nl>
2018-01-27 21:31:40 +0100

    Character movement added

--- Potato Dragon/Assets/Scripts/CharacterScript.cs
+++ Potato Dragon/Assets/Scripts/CharacterScript.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 
 public class CharacterScript : MonoBehaviour {
+    enum Direction { Up, Down, Right, Left }
+    Direction dir;
     public bool target = false;
 
     public int flightNumber = 0;
@@ -12,14 +14,25 @@ public class CharacterScript : MonoBehaviour {
 
     public int currentline = 0;
 
-	// Update is called once per frame
-	void Update () {
+    Vector3 targetPosition = Vector3.zero;
+
+    private void Start()
+    {
+        GetTargetPosition();
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (GetComponentInParent<FlightsScript>().GetDepartureTime(flightNumber) <= 0)
             boarding = true;
         if (suspected && !target )//|| boarding)
             Debug.Log("You Lose");
         else if (target && suspected)
             Debug.Log("You Win");
+        Movement();
+
+        if (Vector3.Distance(transform.position, targetPosition) < 1.5f)
+            GetTargetPosition();
 	}
 
     //Assign a flight to this character
@@ -31,5 +44,109 @@ public class CharacterScript : MonoBehaviour {
     {
         if (Input.GetMouseButtonDown(0) && GetComponentInParent<TextBehaviourScript>().textBox.activeSelf == false)
                 GetComponentInParent<TextBehaviourScript>().ShowText(this);
+    }
+
+    void Movement()
+    {
+        CheckMovementEnd();
+        switch (dir)
+        {
+            case Direction.Left:
+                transform.position += new Vector3(-1, 0, 0) * Time.deltaTime;
+                break;
+            case Direction.Right:
+                transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+                break;
+            case Direction.Up:
+                transform.position += new Vector3(0, 1, 0) * Time.deltaTime;
+                break;
+            case Direction.Down:
+                transform.position += new Vector3(0, -1, 0) * Time.deltaTime;
+                break;
+        }
+    }
+
+    void CheckMovementEnd()
+    {
+        switch (dir)
+        {
+            case Direction.Left:
+                if (transform.position.x <= targetPosition.x)
+                    if (transform.position.y < targetPosition.y)
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(0,0.5f, 0)))
+                            dir = Direction.Up;
+                    }
+                    else
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(0,-0.5f, 0)))
+                            dir = Direction.Down;
+                    }                    
+                break;
+            case Direction.Right:
+                if (transform.position.x >= targetPosition.x)
+                    if (transform.position.y < targetPosition.y)
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(0,0.5f, 0)))
+                            dir = Direction.Up;
+                    }
+                    else
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(0,-0.5f, 0)))
+                            dir = Direction.Down;
+                    }
+                break;
+            case Direction.Up:
+                if (transform.position.y >3.1)
+                    if (transform.position.x < targetPosition.x)
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(0.5f, 0, 0)))
+                            dir = Direction.Right;
+                    }
+                    else
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(-0.5f, 0, 0)))
+                            dir = Direction.Left;
+                    }
+
+                break;
+            case Direction.Down:
+                if (transform.position.y < -0.64f)
+                    if (transform.position.x < targetPosition.x)
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(0.5f, 0, 0)))
+                            dir = Direction.Right;
+                    }
+                    else
+                    {
+                        if (GetComponentInParent<CharactersScript>().LegalPosition(transform.position + new Vector3(-0.5f, 0, 0)))
+                            dir = Direction.Left;
+                    }
+
+                break;
+        }
+    }
+
+    void GetTargetPosition()
+    {
+        targetPosition = Vector3.zero;
+        while (!GetComponentInParent<CharactersScript>().LegalPosition(targetPosition))
+            targetPosition = new Vector3(Random.Range(-4.3f, 4.8f), Random.Range(-1.2f, 3.2f), -5f);
+        if (System.Math.Abs(targetPosition.x - transform.position.x) < 1.6)
+            if (targetPosition.x - transform.position.x > 0.01)
+            {
+                dir = Direction.Right;
+            }
+            else if (targetPosition.x - transform.position.x < 0.01)
+            {
+                dir = Direction.Left;
+            }
+            else
+            {
+                if (targetPosition.y - transform.position.y < 0.01)
+                    dir = Direction.Up;
+                else if (targetPosition.y - transform.position.y > 0.01)
+                    dir = Direction.Down;
+            }
     }
 }
--- Potato Dragon/Assets/Scripts/CharactersScript.cs
+++ Potato Dragon/Assets/Scripts/CharactersScript.cs
@@ -15,7 +15,8 @@ public class CharactersScript : MonoBehaviour {
         for (int i = 0; i < charactersToSpawn; i++) {
             GameObject spawnedCharacter = Instantiate(characterTemplate, transform);
             spawnedCharacter.GetComponent<SpriteRenderer>().sprite = (Sprite)sprites[Random.Range(0,sprites.Length)];
-            spawnedCharacter.transform.position = new Vector3(1, 1, 0) * i + new Vector3(0,0,-5);
+            while(!LegalPosition(spawnedCharacter.transform.position))
+                spawnedCharacter.transform.position = new Vector3(Random.Range(-4.5f,4.8f), Random.Range(-1.2f,3.2f), -5f);
             CharacterScript newCharacter = spawnedCharacter.GetComponent<CharacterScript>();
             spawnedCharacters.Add(newCharacter);
             newCharacter.SetFlight(GetComponent<FlightsScript>().ChooseFlight());
@@ -26,5 +27,9 @@ public class CharactersScript : MonoBehaviour {
     //Choose a random charactrer to be the suspect
     void ChooseTarget() {
         spawnedCharacters[Random.Range(0, spawnedCharacters.Count)].target = true;
+    }
+
+    public bool LegalPosition(Vector3 position) {
+        return !(position.x > -3f && position.x < -1.4f && position.y < 3.1f && position.y > -0.64f) && !(position.x > -0.55f && position.x < 1.05f && position.y < 3.1f && position.y > -0.64f) && !(position.x > 1.8f && position.x < 3.4 && position.y < 3.1f && position.y > -0.64f);
     }
 }
