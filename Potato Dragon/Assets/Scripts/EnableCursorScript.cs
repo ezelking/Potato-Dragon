825ccb63d984ac7998d94396e22a1dd981bad3a3
ezelking-LAPTOP <timroberson@live.nl>
2018-01-27 02:49:10 +0100

    Clickable Character Template added

---
+++ Potato Dragon/Assets/Scripts/EnableCursorScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EnableCursorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //enable cursor
+        Cursor.visible = true;
+    }
+}
